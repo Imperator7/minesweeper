b41e5dacf7a19f2de27b34237d5f00a106f13d4a
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2024-10-15 23:35:00 +0700

    finish basic game's board creation (no hint generator)

---
+++ server/minesweeper.js
@@ -0,0 +1,63 @@
+class Cell {
+  constructor() {
+    this.isRevealed = false
+    this.isFlagged = false
+    this.isMine = false
+    this.hint = 0
+  }
+
+  mine() {
+    this.isMine = true
+  }
+
+  reveal() {
+    this.isRevealed = true
+  }
+
+  flag() {
+    this.isFlagged = !this.isFlagged
+  }
+}
+
+class Minesweeper {
+  constructor(difficulty) {
+    if (difficulty === 'easy') {
+      this.rows = 8
+      this.cols = 10
+      this.mineCount = 10
+      this.board = this.createBoard()
+      this.placeMines()
+    }
+  }
+
+  createBoard() {
+    const board = []
+    for (let row = 0; row < this.rows; row++) {
+      const rowArr = []
+      for (let col = 0; col < this.cols; col++) {
+        rowArr.push(new Cell())
+      }
+      board.push(rowArr)
+    }
+
+    return board
+  }
+
+  placeMines() {
+    const points = new Set()
+    while (points.size < this.mineCount) {
+      let minePoint = JSON.stringify([
+        Math.floor(Math.random() * this.cols),
+        Math.floor(Math.random() * this.rows),
+      ]) // [x,y]
+      points.add(minePoint)
+    }
+
+    points.forEach((point) => {
+      let mine = JSON.parse(point)
+      this.board[mine[1]][mine[0]].mine()
+    })
+  }
+}
+
+module.exports = Minesweeper
