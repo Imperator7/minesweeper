cc6316201812016552e66dad84a30b3055969bde
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2024-10-18 00:47:48 +0700

    Add functionality to send a POST request when cells in grid are clicked

--- client/src/components/minesweeperBoard.jsx
+++ client/src/components/minesweeperBoard.jsx
@@ -1,21 +1,78 @@
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
-export default function game() {
-    useEffect(async () => {
-        try {
-            const res = await axios.get('http://127.0.0.1:4000/')
-            console.log(res)
-        } catch (err) {
-            console.err(err)
-        }
-    }, [])
+export default function Game() {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-    return (
-        <>
-            <div>
-                dwdwqsadasd
-            </div>
-        </>
-    )
+  const gameEndpoint = 'http://127.0.0.1:4000/'
+
+  function createRevealCellConfig(row, col) {
+    return {
+      method: 'post',
+      url: gameEndpoint,
+      data: {
+        row,
+        col,
+      },
+    }
+  }
+
+  async function leftClick(e) {
+    const { target } = e
+    const rowId = target.getAttribute('data-row-id')
+    const colId = target.getAttribute('data-col-id')
+
+    const config = createRevealCellConfig(rowId, colId)
+
+    const res = await axios(config).catch((err) => console.error(err))
+
+    console.log(res)
+  }
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const res = await axios.get(gameEndpoint)
+        setData(res.data)
+      } catch (err) {
+        setError(err.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetch()
+  }, [])
+
+  if (loading) {
+    return <div className="p-4">Loading....</div>
+  }
+
+  if (error) {
+    return <div className="p-4 text-red-500">Error: {error}</div>
+  }
+
+  return (
+    <>
+      <div>Total mines: {data.mineCount} time: 0</div>
+      <div
+        className={`grid grid-cols-${data.cols} grid-rows-${data.rows} m-5 border border-black`}
+      >
+        {console.log(data)}
+        {data.board.map((row, rowindex) =>
+          row.map((cell, colindex) => (
+            <button
+              key={colindex}
+              className="bg-gray-300 shadow-xl min-h-[50px] border border-black "
+              onClick={leftClick}
+              data-row-id={rowindex}
+              data-col-id={colindex}
+            ></button>
+          ))
+        )}
+      </div>
+    </>
+  )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,5 +5,5 @@ export default {
     extend: {},
   },
   plugins: [],
+  safelist: [{ pattern: /^grid-cols-/ }, { pattern: /^grid-rows-/ }],
 }
-
