f22a09254d763875ebf249ae4ab5d1ecaa54e054
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2024-10-22 18:53:16 +0700

    store mine coordinates for further efficiency calculation
    make toJSON method for both Cell and Minesweeper Class to encapsulate some property from frontend
    
    make hint generator method from stored mine coordinates
    
    refactoring some code

--- server/minesweeper.js
+++ server/minesweeper.js
@@ -1,5 +1,7 @@
 class Cell {
-  constructor() {
+  constructor(row, col) {
+    this.row = row
+    this.col = col
     this.isRevealed = false
     this.isFlagged = false
     this.isMine = false
@@ -17,6 +19,20 @@ class Cell {
   flag() {
     this.isFlagged = !this.isFlagged
   }
+
+  addHint() {
+    this.hint += 1
+  }
+
+  toJSON() {
+    return {
+      row: this.row,
+      col: this.col,
+      isRevealed: this.isRevealed,
+      isFlagged: this.isFlagged,
+      hint: this.hint,
+    }
+  }
 }
 
 class Minesweeper {
@@ -26,7 +42,11 @@ class Minesweeper {
       this.cols = 10
       this.mineCount = 10
       this.board = this.createBoard()
+      this.mineCoordinates
+      // should place mines after the first try so there is no way the player gonna hit the mine from the first place.
+      // fix this by add filter cell to place mine and that's it
       this.placeMines()
+      this.generateHint()
     }
   }
 
@@ -35,7 +55,7 @@ class Minesweeper {
     for (let row = 0; row < this.rows; row++) {
       const rowArr = []
       for (let col = 0; col < this.cols; col++) {
-        rowArr.push(new Cell())
+        rowArr.push(new Cell(row, col))
       }
       board.push(rowArr)
     }
@@ -47,15 +67,47 @@ class Minesweeper {
     const points = new Set()
     while (points.size < this.mineCount) {
       let minePoint = JSON.stringify([
-        Math.floor(Math.random() * this.cols),
         Math.floor(Math.random() * this.rows),
+        Math.floor(Math.random() * this.cols),
       ]) // [x,y]
       points.add(minePoint)
     }
 
+    // store mineCoordinates for further calculation
+    this.mineCoordinates = points
+
     points.forEach((point) => {
       let mine = JSON.parse(point)
-      this.board[mine[1]][mine[0]].mine()
+      this.board[mine[0]][mine[1]].mine()
+    })
+  }
+
+  generateHint() {
+    const adjacentCell = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+    ]
+
+    this.mineCoordinates.forEach((mineStr) => {
+      const mine = JSON.parse(mineStr)
+      const [row, col] = mine
+
+      const isValidCell = (row, col) => {
+        return row < this.rows && row >= 0 && col < this.cols && col >= 0
+      }
+
+      for (let [nRow, nCol] of adjacentCell) {
+        const neighborCell = [row + nRow, col + nCol]
+        if (isValidCell(neighborCell[0], neighborCell[1])) {
+          this.board[neighborCell[0]][neighborCell[1]].addHint()
+        }
+      }
     })
   }
 
@@ -71,6 +123,15 @@ class Minesweeper {
       cell.flag()
     }
     return this.board[row][col]
+  }
+
+  toJSON() {
+    return {
+      rows: this.rows,
+      cols: this.cols,
+      mineCount: this.mineCount,
+      board: this.board,
+    }
   }
 }
 
