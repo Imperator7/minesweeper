92a51e7322b84221554f7e4e53fb64b85f08da03
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2024-10-24 17:58:01 +0700

    delete some unnecessary lines

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import MinesweeperBoard from './components/minesweeperBoard'
 
 function App() {
-
   return (
     <>
-      <MinesweeperBoard/>
+      <MinesweeperBoard />
     </>
   )
 }
---
+++ client/src/components/Option.jsx
@@ -0,0 +1,7 @@
+export default function Option() {
+  return (
+    <>
+      <button className="">Try again?</button>
+    </>
+  )
+}
