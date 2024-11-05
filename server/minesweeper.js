f16029458ea9cc2c17ecae803aa57b24046555a9
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2024-11-05 20:32:57 +0700

    refactoring code
    implement recursive method for revealing cells

--- server/minesweeper.js
+++ server/minesweeper.js
@@ -46,13 +46,28 @@ class Minesweeper {
       this.rows = 8
       this.cols = 10
       this.mineCount = 10
+      this.adjacentCells = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+      ]
       this.board = this.createBoard()
       this.mineCoordinates
       // should place mines after the first try so there is no way the player gonna hit the mine from the first place.
       // fix this by add filter cell to place mine and that's it
       this.placeMines()
       this.generateHint()
+      this.visitedCell = new Set()
     }
+  }
+
+  isValidCell(row, col) {
+    return row < this.rows && row >= 0 && col < this.cols && col >= 0
   }
 
   createBoard() {
@@ -88,28 +103,13 @@ class Minesweeper {
   }
 
   generateHint() {
-    const adjacentCell = [
-      [-1, -1],
-      [-1, 0],
-      [-1, 1],
-      [0, -1],
-      [0, 1],
-      [1, -1],
-      [1, 0],
-      [1, 1],
-    ]
-
     this.mineCoordinates.forEach((mineStr) => {
       const mine = JSON.parse(mineStr)
       const [row, col] = mine
 
-      const isValidCell = (row, col) => {
-        return row < this.rows && row >= 0 && col < this.cols && col >= 0
-      }
-
-      for (let [nRow, nCol] of adjacentCell) {
+      for (let [nRow, nCol] of this.adjacentCells) {
         const neighborCell = [row + nRow, col + nCol]
-        if (isValidCell(neighborCell[0], neighborCell[1])) {
+        if (this.isValidCell(neighborCell[0], neighborCell[1])) {
           this.board[neighborCell[0]][neighborCell[1]].addHint()
         }
       }
@@ -122,8 +122,39 @@ class Minesweeper {
       cell.result = 'You lose'
       return cell
     }
+
     cell.reveal()
-    return cell
+    if (cell.hint !== 0) {
+      return [cell]
+    } else {
+      this.visitedCell = new Set()
+      this.visitedCell.add(JSON.stringify([parseInt(row), parseInt(col)]))
+      this.revealAdjacentCells(row, col)
+      console.log(this.visitedCell)
+      const visitedCells = Array.from(this.visitedCell)
+        .map((cell) => JSON.parse(cell))
+        .map(([row, col]) => this.board[row][col])
+      return visitedCells
+    }
+  }
+
+  revealAdjacentCells(row, col) {
+    for (let adjacentCell of this.adjacentCells) {
+      const nRow = parseInt(row) + adjacentCell[0]
+      const nCol = parseInt(col) + adjacentCell[1]
+
+      if (
+        this.isValidCell(nRow, nCol) &&
+        !this.visitedCell.has(JSON.stringify([nRow, nCol]))
+      ) {
+        this.visitedCell.add(JSON.stringify([nRow, nCol]))
+        const nCell = this.board[nRow][nCol]
+        nCell.reveal()
+        if (nCell.hint === 0) {
+          this.revealAdjacentCells(nCell.row, nCell.col)
+        }
+      }
+    }
   }
 
   flagCell(row, col) {
